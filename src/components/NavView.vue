11e2a5a64055c156e3c4790f1c08eb76d84b827f
BEASTxk9 <shanestevensxk9@gmail.com>
2022-08-23 16:19:18 +0200

    update

--- src/components/NavView.vue
+++ src/components/NavView.vue
@@ -1,12 +1,13 @@
 <template>
   <nav>
-    <router-link to="/">Landing</router-link> |
+
       <router-link to="/landing">Home</router-link> |
        <router-link to="/shop">Shop</router-link> |
         <router-link to="/single">Single</router-link> |
          <router-link to="/contact">Contact</router-link> |
           <router-link to="/usersadmin">Users Admin</router-link> |
            <router-link to="/productsadmin">Products Admin</router-link> |
+               <router-link to="/">Logout</router-link> 
            <Cart></Cart>
   </nav>
 </template>
