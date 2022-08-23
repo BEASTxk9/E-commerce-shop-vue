5efc4b7fc2546c3abdfc37061a0db554d3e3e960
BEASTxk9 <shanestevensxk9@gmail.com>
2022-08-23 16:18:48 +0200

    update

--- src/components/LandingNavView.vue
+++ src/components/LandingNavView.vue
@@ -1,8 +1,7 @@
 <template>
     <router-link to="/">Landing</router-link> |
     <router-link to="/register">Register</router-link> |
-     <router-link to="/Login">Login</router-link> |
-      <router-link to="/landing">Home</router-link> |
+     <router-link to="/Login">Login</router-link> 
 </template>
 
 <script>
