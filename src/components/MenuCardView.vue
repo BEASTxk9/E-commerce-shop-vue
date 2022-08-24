18dafb9932d6cb61c0c899c47439e0f80a2999c7
BEASTxk9 <shanestevensxk9@gmail.com>
2022-08-24 16:28:45 +0200

    update

--- src/components/MenuCardView.vue
+++ src/components/MenuCardView.vue
@@ -1,13 +1,32 @@
 <template>
-  <h1>Menu Cards</h1>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-10">
+
+          <div id="card" v-for="(product, index) in products" :key="index">
+            <p>{{ product.Prod_name }} <span></span> R{{ product.price }}</p>
+            <p>{{ product.description }}</p>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
 export default {
-
-}
+  mounted() {
+    this.$store.dispatch("getproducts");
+  },
+  computed: {
+    products() {
+      return this.$store.state.products;
+    },
+  },
+};
 </script>
 
 <style>
-
 </style>
