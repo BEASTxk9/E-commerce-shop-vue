3f28a517bd6016b8d1f308ec0fadcbbd598926be
BEASTxk9 <shanestevensxk9@gmail.com>
2022-08-25 15:37:08 +0200

    update

--- src/components/LandingNavView.vue
+++ src/components/LandingNavView.vue
@@ -1,8 +1,19 @@
 <template>
 <nav class="fixed-top">
-    <router-link to="/">Landing</router-link> |
+
+<div class="row">
+        <div class="col-sm-1">
+ <img id="logo" src="https://i.postimg.cc/wT80tLGN/Rose-Gold-Brush-Glitter-Feminine-Boutique-Circle-Logo-removebg-preview.png" defer>
+    </div>
+
+    <div id="links" class="col-sm-11">
+ <router-link to="/">Landing</router-link> |
     <router-link to="/register">Register</router-link> |
      <router-link to="/Login">Login</router-link> 
+    </div>
+</div>
+
+   
 </nav>
 
 </template>
@@ -15,5 +26,16 @@ export default {
 
 
 <style scoped>
+ #logo{
+height: 90px;
+width: 90px;
+justify-content: start;
+text-align: start;
+align-content: start;
+align-items: start;
+ }
 
+ #links{
+    padding-top: 2rem;
+ }
 </style>
--- src/components/NavView.vue
+++ src/components/NavView.vue
@@ -1,7 +1,12 @@
 <template>
   <nav class="fixed-top">
+<div class="row">
+    <div class="col-sm-1">
+ <img id="logo" src="https://i.postimg.cc/wT80tLGN/Rose-Gold-Brush-Glitter-Feminine-Boutique-Circle-Logo-removebg-preview.png" defer>
+    </div>
 
-      <router-link to="/landing">Home</router-link> |
+    <div id="links" class="col-sm-11">
+     <router-link to="/landing">Home</router-link> |
        <router-link to="/shop">Shop</router-link> |
         <router-link to="/single">Single</router-link> |
           <router-link to="/menu">Menu</router-link> |
@@ -12,6 +17,11 @@
            <router-link to="/productsadmin"><i class="fa-solid fa-bag-shopping"></i></router-link> |
            <!-- logout -->
                <router-link to="/"><i class="fa-solid fa-right-from-bracket"></i></router-link> 
+    </div>
+</div>
+
+
+ 
   
   </nav>
 </template>
@@ -27,5 +37,16 @@ components: {
 </script>
 
 <style scoped>
+ #logo{
+height: 90px;
+width: 90px;
+justify-content: start;
+text-align: start;
+align-content: start;
+align-items: start;
+ }
 
+ #links{
+    padding-top: 2rem;
+ }
 </style>
